2e7667210ef3a15a30c50db747f63545d0f930e8
TeoWeiShan <S10205156@connect.np.edu.sg>
2021-05-24 15:18:41 +0800

    Add project files.

---
+++ WEB2021Apr_P04_T4/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WEB2021Apr_P04_T4.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
