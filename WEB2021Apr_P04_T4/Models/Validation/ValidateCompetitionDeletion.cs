c543cf1edaf949755d94ae4c755eaac8791cd21e
TeoWeiShan <S10205156@connect.np.edu.sg>
2021-07-04 05:44:03 +0800

    Add validation model folder

---
+++ WEB2021Apr_P04_T4/Models/Validation/ValidateCompetitionDeletion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using WEB2021Apr_P04_T4.DAL;
+
+namespace WEB2021Apr_P04_T4.Models.Validation
+{
+    public class ValidateCompetitionDeletion : ValidationAttribute
+    {
+        private CompetitionDAL competitionContext = new CompetitionDAL();
+        protected override ValidationResult IsValid(
+        object value, ValidationContext validationContext)
+        {
+            // Get the email value to validate
+            int competitionID = Convert.ToInt32(value);
+            // Casting the validation context to the "Staff" model class
+            Competition competition = (Competition)validationContext.ObjectInstance;
+            // Get the Staff Id from the staff instance
+            competitionID = competition.CompetitionID;
+            if (competitionContext.IsDeletable(competitionID))
+                // validation failed
+                return new ValidationResult
+                ("Competition cannot be deleted! A competitior has joined!");
+            else
+                // validation passed
+                return ValidationResult.Success;
+        }
+    }
+}
---
+++ WEB2021Apr_P04_T4/Models/Validation/ValidateInterestExists.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using WEB2021Apr_P04_T4.DAL;
+
+namespace WEB2021Apr_P04_T4.Models
+{
+    public class ValidateInterestExists : ValidationAttribute
+    {
+        private AreaInterestDAL areaInterestContext = new AreaInterestDAL();
+        protected override ValidationResult IsValid(
+        object value, ValidationContext validationContext)
+        {
+            // Get the email value to validate
+            string email = Convert.ToString(value);
+            // Casting the validation context to the "Staff" model class
+            AreaInterest areaInterest = (AreaInterest)validationContext.ObjectInstance;
+            // Get the Staff Id from the staff instance
+            int areaInterestID = areaInterest.AreaInterestID;
+            if (areaInterestContext.IsInterestExist(email, areaInterestID))
+                // validation failed
+                return new ValidationResult
+                ("Interest already exists!");
+            else
+                // validation passed
+                return ValidationResult.Success;
+        }
+    }
+}
