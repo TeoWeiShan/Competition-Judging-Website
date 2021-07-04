71057079bf193621b55fb11c0da6065481eed2e1
TeoWeiShan <S10205156@connect.np.edu.sg>
2021-07-05 00:34:19 +0800

    Fix viewCompetition display

--- WEB2021Apr_P04_T4/Models/Competition.cs
+++ WEB2021Apr_P04_T4/Models/Competition.cs
@@ -23,15 +23,18 @@ namespace WEB2021Apr_P04_T4.Models
         public string CompetitionName { get; set; }
 
         [Display(Name = "Start Date")]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime? StartDate { get; set; }
 
         [Display(Name = "End Date")]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime? EndDate { get; set; }
 
         [Display(Name = "Result Released Date")]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime? ResultReleasedDate { get; set; }
     }
 }
