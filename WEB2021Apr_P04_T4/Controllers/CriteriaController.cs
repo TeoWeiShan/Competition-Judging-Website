64c70b7838eb5fc009ff07bcffd1bfedc9cf4a13
4ngelixa <s10205409@connect.np.edu.sg>
2021-07-05 17:15:12 +0800

    add view criteria for judge

---
+++ WEB2021Apr_P04_T4/Controllers/CriteriaController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WEB2021Apr_P04_T4.DAL;
+using WEB2021Apr_P04_T4.Models;
+
+namespace WEB2021Apr_P04_T4.Controllers
+{
+    public class CriteriaController : Controller
+    {
+        private CriteriaDAL criteriaContext = new CriteriaDAL();
+
+        //GET: Criteria
+        public ActionResult Index()
+        {
+            // Stop accessing the action if not logged in
+            // or account not in the "Judge" role
+            if ((HttpContext.Session.GetString("Role") == null) ||
+            (HttpContext.Session.GetString("Role") != "Judge"))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            List<Criteria> criteriaList = criteriaContext.GetAllCriteria();
+            return View(criteriaList);
+        }
+    }
+}
---
+++ WEB2021Apr_P04_T4/Models/Criteria.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WEB2021Apr_P04_T4.Models
+{
+    public class Criteria
+    {
+        [Display(Name = "Criteria ID")]
+        public int CriteriaID { get; set; }
+
+        [Required(ErrorMessage = "Competition ID cannot be null.")]
+        [Display(Name = "Competition ID")]
+        public int CompetitionID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a criteria.")]
+        [Display(Name = "Criteria Name")]
+        public string CriteriaName { get; set; }
+
+        [Required(ErrorMessage = "Please enter a weightage.")]
+        public int Weightage { get; set; }
+    }
+}
