3e38aa02b2c1f8dc5ce38490a9f98ea1847dbb5c
TeoWeiShan <S10205156@connect.np.edu.sg>
2021-07-06 20:47:37 +0800

    Fix editCompetition validation for dates with existing competitors

--- WEB2021Apr_P04_T4/Models/Validation/ValidateCompetitionStart.cs
+++ WEB2021Apr_P04_T4/Models/Validation/ValidateCompetitionStart.cs
@@ -16,14 +16,21 @@ namespace WEB2021Apr_P04_T4.Models.Validation
             // Get the email value to validate
             if (value != null)
             {
-                DateTime startDate = Convert.ToDateTime(value);
-                if (startDate < DateTime.Today)
-                    // validation failed
-                    return new ValidationResult
-                    ("Start Date cannot earlier than Today's Date!");
-                else
-                    // validation passed
-                    return ValidationResult.Success;
+                Competition competition = (Competition)validationContext.ObjectInstance;
+                // Get the Staff Id from the staff instance
+                int competitionID = competition.CompetitionID;
+                if (!competitionContext.IsModifiable(competitionID))
+                {
+                    DateTime startDate = Convert.ToDateTime(value);
+                    if (startDate < DateTime.Today)
+                        // validation failed
+                        return new ValidationResult
+                        ("Start Date cannot earlier than Today's Date!");
+                    else
+                        // validation passed
+                        return ValidationResult.Success;
+                }
+                else { return ValidationResult.Success; }
             }
             else { return ValidationResult.Success; }
         }
