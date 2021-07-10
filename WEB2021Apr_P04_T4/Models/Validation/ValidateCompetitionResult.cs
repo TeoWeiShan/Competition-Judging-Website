e82a775aa4b8cd8f2f3fe03537bba89a124509fe
TeoWeiShan <S10205156@connect.np.edu.sg>
2021-07-11 01:31:06 +0800

    Fix competition date validation

--- WEB2021Apr_P04_T4/Models/Validation/ValidateCompetitionResult.cs
+++ WEB2021Apr_P04_T4/Models/Validation/ValidateCompetitionResult.cs
@@ -21,15 +21,30 @@ namespace WEB2021Apr_P04_T4.Models.Validation
                 if (endDate == null)
                     return new ValidationResult
                     ("End Date is needed to enter values for Result End Date");
+                else if (resultDate > DateTime.Today.AddYears(7))
+                    return new ValidationResult
+                           ("Result Release Date cannot be 7 years later than Today's Date!");
                 else if (resultDate == endDate)
                     // validation failed
                     return new ValidationResult
                     ("Result End Date cannot be on the same day as End Date!");
-                else if (resultDate <= endDate)
+                else if (resultDate < endDate)
                     // validation failed
                     return new ValidationResult
                     ("Result End Date cannot be earlier than End Date!");
                 else
+                {
+                    if (competition.StartDate == null)
+                    {
+                        return new ValidationResult
+                        ("Start Date cannot be null!");
+                    }
+                    else
+                    {
+                        // validation passed
+                        return ValidationResult.Success;
+                    }
+                }
                     // validation passed
                     return ValidationResult.Success;
             }
