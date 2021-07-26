30322b77e9fe9f913487002dca6d4a75f5943fe5
TeoWeiShan <S10205156@connect.np.edu.sg>
2021-07-27 00:34:09 +0800

    Add CompetitionJudge View & Add Judge

--- WEB2021Apr_P04_T4/Models/CompetitionJudge.cs
+++ WEB2021Apr_P04_T4/Models/CompetitionJudge.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using WEB2021Apr_P04_T4.Models.Validation;
 
 namespace WEB2021Apr_P04_T4.Models
 {
     public class CompetitionJudge
     {
+        [ValidateCompetitionJudgeModification]
         [Display(Name = "ID")]
         public int CompetitionID { get; set; }
 
         [Required]
-        public string JudgeID { get; set; }
+        public int JudgeID { get; set; }
+
+
     }
 }
