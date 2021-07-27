96cfd98927e624cac06350f8429c5cc1a2dc75ec
4ngelixa <s10205409@connect.np.edu.sg>
2021-07-27 13:25:59 +0800

    add CompetitionSubmission for Judge

--- WEB2021Apr_P04_T4/Models/CompetitionSubmissionViewModel.cs
+++ WEB2021Apr_P04_T4/Models/CompetitionSubmissionViewModel.cs
@@ -6,7 +6,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace WEB2021Apr_P04_T4.Models
 {
-    public class CompetitionSubmission
+    public class CompetitionSubmissionViewModel
     {
         [Display(Name = "Competition ID")]
         public int CompetitionID { get; set; }
@@ -15,16 +15,21 @@ namespace WEB2021Apr_P04_T4.Models
         public int CompetitorID { get; set; }
 
         [Display(Name = "File submitted")]
-        public string? FileSubmitted { get; set; }
+        public char FileSubmitted { get; set; }
 
         [Display(Name = "Date/Time of submission")]
-        public DateTime? DateTimeFileUpload { get; set; }
+        public DateTime DateTimeFileUpload { get; set; }
 
-        public string? Appeal { get; set; }
+        public char Appeal { get; set; }
 
         [Display(Name = "Vote Count")]
         public int VoteCount { get; set; }
 
-        public int? Ranking { get; set; }
+        public int Ranking { get; set; }
+
+        [Display(Name = "Criteria ID")]
+        public int CriteriaID { get; set; }
+               
+        public int Score { get; set; }
     }
 }
