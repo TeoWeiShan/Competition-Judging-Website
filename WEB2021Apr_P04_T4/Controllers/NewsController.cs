868a0be8751c8f8d487c926fef18df65a171446d
TeoWeiShan <S10205156@connect.np.edu.sg>
2021-07-27 21:08:06 +0800

    Add View News

---
+++ WEB2021Apr_P04_T4/Controllers/NewsController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WEB2021Apr_P04_T4.Models;
+
+namespace WEB2021Apr_P04_T4.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET: NewsController
+        public async Task<ActionResult> Index()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://weblcu-f967.restdb.io");
+            HttpResponseMessage response = await client.GetAsync("/rest/news?apikey=60ffe4f149cd3a5cfbd22c73&sort=date&dir=-1");
+            if (response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+                List<News> newsList = JsonConvert.DeserializeObject<List<News>>(data);
+                return View(newsList);
+            }
+            else
+            {
+                return View(new List<News>());
+            }
+        }
+
+            // GET: NewsController/Details/5
+            public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: NewsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: NewsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NewsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: NewsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NewsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: NewsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WEB2021Apr_P04_T4/Models/News.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB2021Apr_P04_T4.Models
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public string Header { get; set; }
+        public DateTime Date { get; set; }
+        public string Intro { get; set; }
+        public string Content { get; set; }
+    }
+}
