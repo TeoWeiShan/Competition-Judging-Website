735609fc3ace7d21aa0918d5c7c1964d0341e0b7
4ngelixa <s10205409@connect.np.edu.sg>
2021-07-27 20:15:18 +0800

    add CompetitionSubmission controllers, DAL, views

--- WEB2021Apr_P04_T4/Controllers/CompetitionScoreController.cs
+++ WEB2021Apr_P04_T4/Controllers/CompetitionScoreController.cs
@@ -4,15 +4,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WEB2021Apr_P04_T4.DAL;
+using WEB2021Apr_P04_T4.Models;
+
 
 namespace WEB2021Apr_P04_T4.Controllers
-{
-    public class CompetitionScore : Controller
+{   
+    public class CompetitionScoreController : Controller
     {
+        private CompetitionScoreDAL scoreContext = new CompetitionScoreDAL();
+
         // GET: CompetitionScore
         public ActionResult Index()
         {
-            return View();
+            // Stop accessing the action if not logged in
+            // or account not in the "Judge" role
+            if ((HttpContext.Session.GetString("Role") == null) ||
+            (HttpContext.Session.GetString("Role") != "Judge"))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            List<CompetitionScore> scoreList = scoreContext.GetAllScore();
+            return View(scoreList);
         }
 
         // GET: CompetitionScore/Details/5
--- WEB2021Apr_P04_T4/DAL/CompetitionSubmissionDAL.cs
+++ WEB2021Apr_P04_T4/DAL/CompetitionSubmissionDAL.cs
@@ -28,12 +28,13 @@ namespace WEB2021Apr_P04_T4.DAL
             conn = new SqlConnection(strConn);
         }
 
-        public List<CompetitionSubmission> GetAllSubmission()
+        public List<CompetitionSubmission> GetAllSubmission(int JudgeID)
         {
             //Create a SqlCommand object from connection object
             SqlCommand cmd = conn.CreateCommand();
             //Specify the SELECT SQL statement
-            cmd.CommandText = @"SELECT * FROM CompetitionSubmission ORDER BY CompetitionID";
+            cmd.CommandText = @"select * from CompetitionSubmission Where competitionid = (select CompetitionID from CompetitionJudge where JudgeID = @selectedJudgeID)";
+            cmd.Parameters.AddWithValue("@selectedJudgeID", JudgeID);
             //Open a database connection
             conn.Open();
             //Execute the SELECT SQL through a DataReader
@@ -42,7 +43,7 @@ namespace WEB2021Apr_P04_T4.DAL
             List<CompetitionSubmission> submissionList = new List<CompetitionSubmission>();
 
             while (reader.Read())
-            {
+            { //ifreaderhasrows
                 submissionList.Add(
                 new CompetitionSubmission
                 {
@@ -65,6 +66,49 @@ namespace WEB2021Apr_P04_T4.DAL
             return submissionList;
         }
 
+        public CompetitionSubmission GetDetails(int competitionId)
+        {
+            CompetitionSubmission submission = new CompetitionSubmission();
+
+            //Create a SqlCommand object from connection object
+            SqlCommand cmd = conn.CreateCommand();
+
+            //Specify the SELECT SQL statement that 
+            //retrieves all attributes of a staff record.
+            cmd.CommandText = @"SELECT * FROM CompetitionSubmission WHERE CompetitionID = @selectedCompetitionID";
+
+            //Define the parameter used in SQL statement, value for the
+            //parameter is retrieved from the method parameter “staffId”.
+            cmd.Parameters.AddWithValue("@selectedCompetitionID", competitionId);
+
+            //Open a database connection
+            conn.Open();
+
+            //Execute SELCT SQL through a DataReader
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                //Read the record from database
+                while (reader.Read())
+                {
+                    // Fill staff object with values from the data reader 
+                    submission.CompetitionID = competitionId;
+                    submission.CompetitorID = reader.GetInt32(1);
+                    submission.FileSubmitted = reader.IsDBNull(2) ? null : reader.GetString(2);
+                    submission.DateTimeFileUpload = !reader.IsDBNull(3) ? reader.GetDateTime(3) : (DateTime?)null;
+                    submission.Appeal = reader.IsDBNull(4) ? null : reader.GetString(4);
+                    submission.VoteCount = reader.GetInt32(5);
+                    submission.Ranking = !reader.IsDBNull(6) ? reader.GetInt32(6) : (int?)null;
+                }
+            }
+
+            //Close DataReader
+            reader.Close();
+            //Close Database connection
+            conn.Close();
+
+            return submission;
+        }
     }
 }
 
